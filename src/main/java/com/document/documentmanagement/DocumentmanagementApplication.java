dca593a287819b512737e944fbc1e881e4264937
VyshnaviVara <thimmayyagariv@gmail.com>
2025-04-07 22:05:34 +0530

    added files

---
+++ src/main/java/com/document/documentmanagement/DocumentmanagementApplication.java
@@ -0,0 +1,13 @@
+package com.document.documentmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocumentmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocumentmanagementApplication.class, args);
+	}
+
+}
